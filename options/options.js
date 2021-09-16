c110b91c3f4d21ade56320de685e9d15d757b303
Crowdbotics <team@crowdbotics.com>
2021-09-16 08:03:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lucky_waterfall_30589Identifier",
+  url: "https://lucky_waterfall_30589Identifier.botics.co",
+  api: "https://lucky_waterfall_30589Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
